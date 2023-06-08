0b9c1ea03ed412cfc424993277b117b45db36e8e
Shreya <shreya.r.darekar@gmail.com>
2023-06-08 16:06:21 -0700

    Solved Phase 1

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -6,10 +6,14 @@ class KeyValuePair {
   }
 }
 
-class HashTable { // get O(1), set O(1), deleteKey O(1)
+class HashTable {
+  // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
     // Initialize your buckets here
+    this.data = new Array(numBuckets).fill(null);
+    this.capacity = numBuckets;
+    this.count = 0;
     // Your code here
   }
 
@@ -28,26 +32,68 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
     return this.hash(key) % this.capacity;
   }
 
-
   insert(key, value) {
     // Your code here
+    const index = this.hashMod(key);
+    let currNode = this.data[index];
+    while (currNode && currNode.key !== key) {
+      currNode = currNode.next;
+    }
+    if (currNode) {
+      currNode.value = value;
+    } else {
+      const newNode = new KeyValuePair(key, value);
+      if (!this.data[index]) {
+        this.data[index] = newNode;
+      } else {
+        newNode.next = this.data[index];
+        this.data[index] = newNode;
+      }
+      this.count++;
+    }
   }
-
 
   read(key) {
     // Your code here
+    const index = this.hashMod(key);
+    let currNode = this.data[index];
+    while (currNode) {
+      if (currNode.key === key) {
+        return currNode.value;
+      }
+      currNode = currNode.next;
+    }
   }
-
 
   resize() {
     // Your code here
+    const dataCopy = this.data.slice();
+    this.capacity = this.capacity * 2;
+    this.count = 0;
+    this.data = new Array(this.capacity).fill(null);
+    dataCopy.forEach((KeyValuePair) => {
+      let curr = KeyValuePair;
+      while (curr) {
+        this.insert(curr.key, curr.value);
+        curr = curr.next;
+      }
+    });
   }
-
 
   delete(key) {
     // Your code here
+    const index = this.hashMod(key);
+    let currNode = this.data[index];
+    while (currNode) {
+      if (currNode.key === key) {
+        currNode.key = undefined;
+        currNode.value = undefined;
+        this.count--;
+      }
+      currNode = currNode.next;
+    }
+    return "Key not found";
   }
 }
-
 
 module.exports = HashTable;
