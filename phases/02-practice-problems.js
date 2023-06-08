562272c41b1df836e8bef92fbdbeb62215c5014c
Bailey <bjohnson82619@gmail.com>
2023-06-08 11:31:46 -0500

    solutions to phase2

--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,20 +1,45 @@
 function anagrams(str1, str2) {
-  // Your code here
+  const str1Set = new Set (str1);
+  for(let i = 0; i < str2.length; i++){
+    let char = str2[i]
+    if(!str1Set.has(char)){
+      return false;
+    }
+  }
+  return true;
 }
 
 
 function commonElements(arr1, arr2) {
-  // Your code here
+  const arr1Set = new Set(arr1)
+  return arr2.filter((el) => arr1Set.has(el))
 }
 
 
 function duplicate(arr) {
-  // Your code here
+  const setArr = new Set ()
+  for(let i = 0; i < arr.length; i++){
+    let el = arr[i];
+    if(setArr.has(el)){
+      return el;
+    }
+    setArr.add(el)
+  }
 }
 
 
 function twoSum(nums, target) {
-  // Your code here
+  const setArr = new Set ()
+  for(let i = 0; i < nums.length; i++){
+    let el = nums[i]
+    const sub = target - el
+    if(setArr.has(sub)){
+      return true;
+    } else {
+      setArr.add(el)
+    }
+  }
+  return false;
 }
 
 
